87df05cd00f4ce8cf0fba8d91320fd16346d04fb
Nilpo <devnilpo@gmail.com>
2014-05-31 01:59:23 -0400

    adding license text

--- repo.php
+++ repo.php
@@ -13,4 +13,20 @@ Author URI: http://robdunham.info/
 License: GPL2
 */
 
+/*  Copyright 2014  Rob "Nilpo" Dunham  (email : contact@robdunham.info)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
