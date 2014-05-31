05abf3e8636eb8fd005a7dcb4eabaed81b13688d
Nilpo <devnilpo@gmail.com>
2014-05-31 01:49:32 -0400

    cleaning up plugin information and adding license

--- repo.php
+++ repo.php
@@ -7,9 +7,10 @@
 Plugin Name: WP Repo
 Plugin URI: 
 Description: <cite>WP Repo</cite> Is a simple plugin that implements repo.js for integrating GitHub repositories into Wordpress posts.
-Author: Rob "Nilpo" Dunham
 Version: 0.1
+Author: Rob "Nilpo" Dunham
 Author URI: http://robdunham.info/
+License: GPL2
 */
 
 
