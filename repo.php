1928feb930738c4433fab502067c76f4e3b1d70f
Nilpo <devnilpo@gmail.com>
2014-05-30 23:13:03 -0400

    Initializing empty Wordpress plugin

---
+++ repo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package WP-Repo
+ * @version 0.1
+ */
+/*
+Plugin Name: WP Repo
+Plugin URI: 
+Description: <cite>WP Repo</cite> Is a simple plugin that implements repo.js for integrating GitHub repositories into Wordpress posts.
+Author: Rob "Nilpo" Dunham
+Version: 0.1
+Author URI: http://robdunham.info/
+*/
+
+
