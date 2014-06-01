0d0f5d45f4667805d6d45190f5c6ddbcd87e8968
Nilpo <devnilpo@gmail.com>
2014-06-01 01:16:10 -0400

    adding javascript to enable Repo.js on page elements

---
+++ wp-repo.js
@@ -0,0 +1,16 @@
+$(window).load(function() {
+  $('.repo').each(function() {
+    //$(this).attr('data-name');    //legacy
+    data = {
+      user:   $(this).data('user'),
+      name:   $(this).data('name')
+    }
+    
+    branch = $(this).data('branch');
+    if (branch !== undefined) {
+      data.branch = branch;
+    }
+    
+    $(this).repo(data);
+  });
+});
